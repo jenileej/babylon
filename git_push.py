bce76c1973117538015f1f2696236ba644975138
jenilee <jenilee@painscansystem.com>
2022-02-06 22:31:53 -0700

    commit message 2022-02-06 22:31:53.245092

--- git_push.py
+++ git_push.py
@@ -5,6 +5,6 @@ today = str(datetime.datetime.now())
 
 repo = Repo('')
 repo.git.add('--all')
-repo.git.commit('-m', 'commit message' + today)
+repo.git.commit('-m', 'commit message ' + today)
 origin = repo.remote(name='origin')
 origin.push()
